912b7d6ad61adb21466d283909d8a87a3a67a1c1
lposse <lucas.cplago@gmail.com>
2025-02-25 07:55:51 +0100

    test2

--- get_next_line.c
+++ get_next_line.c
@@ -1,4 +1,43 @@
 
 #include <unistd.h>
+#include <stdlib.h>
 
+char	*ft_line(ssize_t bytes_read, static char *buff)
+{
+	char	*result;
+	size_t	i;
 
+	i = 0;
+	while (bytes_read >= i)
+	{
+		if (buff[i] == '\n' || buff[i] == '\0')
+			break ;
+		else
+			result[i] = buff[i];
+	}
+	i++;
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buff;
+	char	*line;
+	char	*temp;
+	ssize_t		bytes_read;
+	
+	if (!buff)
+		buff = malloc((sizeof(buff) + 1) * sizeof(char));
+	if (!buff)
+		return (NULL);
+	while (bytes_read)
+	{
+		bytes_read = read(fd, buff, sizeof(buff));
+		if (bytes_read < 0)
+			return (NULL);
+		else
+			line = ft_line(bytes_read, buff);
+	}
+	temp = line;
+	free(line);
+	return (temp)
+}
